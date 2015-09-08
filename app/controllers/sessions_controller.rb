25a9a5efb86bfd1ba3cd1fe6eb9406ba1c1170de
Andy Edwards <andyhedwards@gmail.com>
2015-09-08 13:58:14 +1200

    Add facebook login
    Add omniauth-facebook gem. Create landing controller. Create User model.
    Add login routes.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def create
+    auth = request.env["omniauth.auth"]
+    session[:omniauth] = auth.expect('extra')
+    user = User.sign_in_from_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "Signed In"
+  end
+
+  def auth_hash
+    request.env["omniauth.auth"]
+  end
+
+  def destroy
+    session[:user_id] = nil
+    session[:omniauth] = nil
+    redirect_to root_url, notice: "Signed Out"
+  end
+end
