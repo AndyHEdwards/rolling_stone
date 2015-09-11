11d99572fc1311683c5fd9616914c64b29089b5e
Andy Edwards <andyhedwards@gmail.com>
2015-09-11 17:24:19 +1200

    Try to implement facebook specific login but didnt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Rolling Stone!"
-
+      session[:user_id] = @user.id
       redirect_to journal_index_path
     else
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-
-
 
 
   def self.sign_in_from_omniauth(auth)
